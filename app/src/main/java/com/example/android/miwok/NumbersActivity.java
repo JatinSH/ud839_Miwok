1c95af628c1b9a9129ec6bfa23fd7fc15abeb0d2
JatinSH <goeljatin28@gmail.com>
2019-08-25 20:26:29 +0530

    Printed numbers through one to ten on NumbersActivity using while loop.

--- app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -17,6 +17,9 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 
@@ -42,5 +45,17 @@ public class NumbersActivity extends AppCompatActivity {
         words.add("eight");
         words.add("nine");
         words.add("ten");
+
+        LinearLayout rootView= (LinearLayout) findViewById(R.id.rootView);
+
+        int index=0;
+        while(index<words.size()) {
+
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(index));
+            rootView.addView(wordView);
+            index++;
+        }
+
     }
 }
